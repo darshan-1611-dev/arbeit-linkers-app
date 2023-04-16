79a846bdaad4b6797dfa68060a3ea9dce16d15db
darshan <dhananid90@gmail.com>
2023-04-16 15:26:19 +0530

    update in payment button design

--- resources/views/payment_view.blade.php
+++ resources/views/payment_view.blade.php
@@ -1,3 +1,25 @@
+<style>
+    .razorpay-payment-button{
+        display: inline-block;
+        font-weight: 400;
+        line-height: 1.5;
+        text-align: center;
+        text-decoration: none;
+        vertical-align: middle;
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+        padding: 0.375rem 0.75rem;
+        font-size: 1rem;
+        border-radius: 0.25rem;
+        transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+        color: #fff;
+        background-color: #dc3545;
+        border: 1px solid #dc3545;
+    }
+</style>
+
 <form action="{{ route('razorpay.payment.store') }}" method="POST">
     @csrf
     <script src="https://checkout.razorpay.com/v1/checkout.js"
@@ -12,12 +34,6 @@
             data-prefill.email="email"
             data-theme.color="#090B3D">
     </script>
-
-    <script>
-        // Hide default stripe button, be careful there
-        document.getElementsByClassName("razorpay-payment-button")[0].style.display = 'none';
-    </script>
-    <button type="submit" class="btn btn-danger">Pay Now</button>
 
     <input type="hidden" name="job_id" value="{{ $job_id }}">
     <input type="hidden" name="receiver_id" value="{{ $receiver_id }}">
